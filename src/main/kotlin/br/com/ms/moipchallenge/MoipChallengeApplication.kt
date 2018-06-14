19c9b9bb0fae057bdfdeccd35adb440a44ff6d7b
Mario Alvial <mse.alvial@gmail.com>
2018-06-14 12:58:37 -0300

    Add Sentry

--- src/main/kotlin/br/com/ms/moipchallenge/MoipChallengeApplication.kt
+++ src/main/kotlin/br/com/ms/moipchallenge/MoipChallengeApplication.kt
@@ -1,10 +1,20 @@
 package br.com.ms.moipchallenge
 
+import io.sentry.spring.SentryExceptionResolver
+import io.sentry.spring.SentryServletContextInitializer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class MoipChallengeApplication
+class MoipChallengeApplication{
+
+    @Bean
+    fun sentryExceptionResolver() = SentryExceptionResolver()
+
+    @Bean
+    fun sentryServletContextInitializer() = SentryServletContextInitializer()
+}
 
 fun main(args: Array<String>) {
     runApplication<MoipChallengeApplication>(*args)
