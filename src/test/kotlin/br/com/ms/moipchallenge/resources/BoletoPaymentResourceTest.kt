647e528ab453ffe082b7e97ba9fa927db8c28b0b
Mario Alvial <mse.alvial@gmail.com>
2019-01-09 22:52:50 -0200

    Add resources tests

---
+++ src/test/kotlin/br/com/ms/moipchallenge/resources/BoletoPaymentResourceTest.kt
@@ -0,0 +1,35 @@
+package br.com.ms.moipchallenge.resources
+
+import br.com.ms.moipchallenge.models.BoletoPayment
+import br.com.ms.moipchallenge.models.Buyer
+import br.com.ms.moipchallenge.models.Client
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.module.SimpleModule
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
+import java.math.BigDecimal
+
+class BoletoPaymentResourceTest {
+
+    lateinit var mapper: ObjectMapper
+
+    @Before
+    fun setup() {
+        mapper = ObjectMapper()
+        mapper.registerModule(SimpleModule().addSerializer(BoletoPayment::class.java, BoletoPaymentResource()))
+    }
+
+    @Test
+    fun givenValidBoletoPaymentShouldSerializeItCorrectly() {
+        val client = Client(0)
+        val buyer = Buyer("Buyer", "buyer@gmail.com", "65914918057")
+        val boletoPayment = BoletoPayment(BigDecimal(35.3), client, buyer)
+
+        val json = "${mapper.writeValueAsString(boletoPayment).substringBefore(",\"_links")}}"
+
+        assertThat(json).isEqualTo(responseJson(boletoPayment.number))
+    }
+
+    private fun responseJson(number: String) = "{\"id\":0,\"amount\":35.30,\"type\":\"BOLETO\",\"number\":\"$number\",\"client\":{\"id\":0,\"name\":\"Client 0\"},\"buyer\":{\"id\":0,\"name\":\"Buyer\",\"email\":\"buyer@gmail.com\",\"cpf\":\"65914918057\"}}"
+}
---
+++ src/test/kotlin/br/com/ms/moipchallenge/resources/CardPaymentResourceTest.kt
@@ -0,0 +1,45 @@
+package br.com.ms.moipchallenge.resources
+
+import br.com.ms.moipchallenge.enums.CardPaymentStatus.APPROVED
+import br.com.ms.moipchallenge.models.Buyer
+import br.com.ms.moipchallenge.models.Card
+import br.com.ms.moipchallenge.models.CardPayment
+import br.com.ms.moipchallenge.models.Client
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.module.SimpleModule
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
+import java.math.BigDecimal
+import java.time.LocalDate
+
+class CardPaymentResourceTest {
+
+    lateinit var mapper: ObjectMapper
+
+    @Before
+    fun setup() {
+        mapper = ObjectMapper()
+        mapper.registerModule(SimpleModule().addSerializer(CardPayment::class.java, CardPaymentResource()))
+    }
+
+    @Test
+    fun givenValidBoletoPaymentShouldSerializeItCorrectly() {
+        val client = Client(0)
+        val buyer = Buyer("Buyer", "buyer@gmail.com", "65914918057")
+        val card = Card("Holder", "5339456341711112", LocalDate.of(2030, 3, 14), "181")
+        val cardPayment = CardPayment(BigDecimal(35.3), client, buyer, card, APPROVED)
+
+        val json = format(mapper.writeValueAsString(cardPayment)).substringBefore(",\"_links")
+
+        assertThat(json).isEqualTo(responseJson())
+    }
+
+    private fun format(json: String): String {
+        val begin = json.substringBefore("expirationDate")
+        val end = json.substringAfter("cvv")
+        return "${begin}cvv$end"
+    }
+
+    private fun responseJson() = "{\"id\":0,\"amount\":35.30,\"type\":\"CREDIT_CARD\",\"status\":\"APPROVED\",\"card\":{\"holderName\":\"Holder\",\"number\":\"5339456341711112\",\"cvv\":\"181\"},\"client\":{\"id\":0,\"name\":\"Client 0\"},\"buyer\":{\"id\":0,\"name\":\"Buyer\",\"email\":\"buyer@gmail.com\",\"cpf\":\"65914918057\"}"
+}
